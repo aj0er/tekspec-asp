9dfeb841d4df5240f397e6407a5f3bdc2d19b987
aj0er <ajoeaar@gmail.com>
2022-08-29 09:34:37 +0200

    Initial commit, add template code

---
+++ ExampleForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExampleForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
